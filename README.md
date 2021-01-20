c906318e57d0d65833aeddeccb049ed9f0e3866b
Shimon Wosner <31377730+shwosner@users.noreply.github.com>
2021-01-20 06:38:10 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Real-time Chat with SSE (Server Sent Events)
+
+This is an example application which uses vanilla _Server Sent Events_ for real-time chat implementation without web sockets and long looping.
+
+# Install
+
+Important note: This app can only run on node v14.15.4 or higher.
+
+`npm install` to setup dependencies
+
+# Run
+
+`npm run dev` to run server on port 4000 with _nodemon_
+`npm start` to run server on port 4000
+
+Open `client.html` in your browser (IE not supported) in several tabs and try to type and send some text.
+
+# Example
+
+!['example'](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEMu_7Zrv8wPEw2zvfLT3FOcYBjNJCOy1bKQ&usqp=CAU "example")
---
+++ server.js
@@ -0,0 +1,80 @@
+import express from "express";
+import compression from "compression";
+
+const app = express();
+
+app.use(express.json());
+app.use(compression());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+let connectionId = 0;
+let connections = 0;
+
+const messages = [{ message: "message1", id: Date.now() }];
+
+app.get("/stream", (req, res, next) => {
+  // connections.push(res);
+  if (connections === 10) {
+    console.log(
+      "Max 10 connections, (Use emitter.setMaxListeners() to increase limit)"
+    );
+    return res.status(500).send("Max 10 clients on the same time :-(");
+  }
+  connectionId++;
+  connections++;
+  res.set({
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+    Connection: "keep-alive",
+    "Access-Control-Allow-Origin": "*",
+  });
+  console.log(`client ${connectionId} connected to SSE`);
+  // console.log("connections :>> ", connections);
+  res.write(`event: connected\n`);
+  res.status(200).write(`data: Connected to stream\n\n`);
+  res.flush();
+
+  // setInterval(function () {
+  //   if (localVersion < globalVersion) {
+  //     console.log("sending stream");
+  //     res.status(200).write(`data: ${JSON.stringify(messages)}\n\n`);
+  //     localVersion = globalVersion;
+  //     res.flush();
+  //   }
+  // }, 100);
+
+  app.on("message", (data) => {
+    // console.log("data :>> ", data);
+    res.write(`event: message\n`);
+    res.write(`data: ${JSON.stringify(data)}\n\n`);
+    res.flush();
+  });
+
+  req.on("close", () => {
+    console.log("client closed,");
+    connections--;
+    console.log(`Currently ${connections} connections`);
+  });
+});
+
+app.post("/add_message", (req, res) => {
+  // console.log("req.body :>> ", req.body);
+  const { message } = req.body;
+  messages.push({ message, id: Date.now() });
+
+  app.emit("message", {
+    title: "New message!",
+    message,
+    messages,
+    timestamp: new Date(),
+  });
+  res.status(200).json({ message: "Success" }).end();
+});
+app.get("/", (req, res) => {
+  res.send(messages);
+});
+app.listen(4000, function () {
+  console.log("Example app listening on port 4000!");
+});
